26ec429b5223256cf89fa8fb2a9743a71c6a6f67
zackteo <zackteo@gmail.com>
2021-06-21 22:12:02 +0800

    Attempt using reagent reaction

--- src/leaflet_example/core.cljs
+++ src/leaflet_example/core.cljs
@@ -2,6 +2,7 @@
   (:require
    [reagent.core :as r]
    [reagent.dom :as d]
+   [reagent.ratom]
    ["leaflet" :as leaflet]
    ["react-leaflet" :as react-leaflet]))
 
@@ -13,25 +14,46 @@
 
 (def MapContainer (r/adapt-react-class react-leaflet/MapContainer))
 (def TileLayer (r/adapt-react-class react-leaflet/TileLayer))
+(def GeoJSON (r/adapt-react-class react-leaflet/GeoJSON))
+
 (def default-center [1.352 103.851])
 
-(def state (r/atom 0))
+(def state (r/atom nil
+                   ;; (clj->js
+                   ;;   {:type "MultiPolygon"
+                   ;;    :crs {:type "name"
+                   ;;          :properties {:name "EPSG:3857"}}
+                   ;;    :coordinates [[[[103.72881064,1.381712426],[103.728247365,1.380988051]
+                   ;;                    [103.727701114,1.38028557],[103.726068963,1.381602655]
+                   ;;                    [103.726049687,1.38161821],[103.727219458,1.383022459]
+                   ;;                    [103.727259673,1.382986286],[103.72881064,1.381712426]]]]})
+                   ))
+
+(def other-geojson (clj->js {:type "MultiPolygon"
+                             :coordinates [[[[104.015476006,1.400841267]
+                                             [104.073266125,1.404366425]
+                                             [104.075302033,1.367916928]
+                                             [104.080940674,1.337752011]
+                                             [104.015476006,1.400841267]]]]}))
 
 (defn leaflet-map []
-  (fn []
-    [MapContainer
-     {:center default-center :zoom 12 ;; :scrollWheelZoom false
-      :style {:width "1000px" :height "1000px"}}
-
-     [TileLayer
-      {:url osm-url
-       :attribution copy-osm}]]))
+  (let [data (reagent.ratom/reaction (true? @state))]
+    (fn []
+      [MapContainer
+       {:center default-center :zoom 12 ;; :scrollWheelZoom false
+        :style {:width "1000px" :height "1000px"}}
+       [TileLayer
+        {:url osm-url
+         :attribution copy-osm}]
+       [GeoJSON
+        {:data @data}]])))
 
 (defn button []
   (fn []
     [:button.green
-     {:on-click #(swap! state inc)}
+     {:on-click #(reset! state other-geojson)}
      (str @state)]))
+
 
 ;; -------------------------
 ;; Initialize app
