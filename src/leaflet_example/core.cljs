63c33776142078bf13b59e2bc2b126c1068c3cb8
zackteo <zackteo@gmail.com>
2021-06-21 21:19:25 +0800

    Simple leaflet map display

---
+++ src/leaflet_example/core.cljs
@@ -0,0 +1,42 @@
+(ns leaflet-example.core
+  (:require
+   [reagent.core :as r]
+   [reagent.dom :as d]
+   ["leaflet" :as leaflet]
+   ["react-leaflet" :as react-leaflet]))
+
+;; -------------------------
+;; Views
+
+(def copy-osm "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors")
+(def osm-url (str js/window.location.protocol "//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"))
+
+(def MapContainer (r/adapt-react-class react-leaflet/MapContainer))
+(def TileLayer (r/adapt-react-class react-leaflet/TileLayer))
+(def default-center [1.352 103.851])
+
+(defn leaflet-map []
+  (fn []
+    [MapContainer
+     {:center default-center :zoom 12 ;; :scrollWheelZoom false
+      :style {:width "1000px" :height "1000px"}}
+
+     [TileLayer
+      {:url osm-url
+       :attribution copy-osm}]]))
+
+;; -------------------------
+;; Initialize app
+
+
+(defn mount-root []
+  (d/render [leaflet-map] (.getElementById js/document "app")))
+
+;; (defn mount-root []
+;;   (d/render
+;;     [:div
+;;      ;; {:style {:height "100%"}}
+;;      [home-page]] (.getElementById js/document "app")))
+
+(defn ^:export init! []
+  (mount-root))
